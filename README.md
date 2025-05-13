65fbf959ec98c2af1f5f063d2dd2607c113803cd
Sumair Shaikh <shaikhsumair01@gmail.com>
2025-05-13 22:42:27 +0530

    added geolocation and dropdown list

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# Weather_app
+Created a weather application using the WeatherApi. Initially I was trying to implement the openweathermap.api but due to some technical issues, was unable to access the api so shifted to WeatherApi.
+ The application on browser load initially displays the weather forecast in mumbai, India. When the user enters a location in the search bar, the application will search for the specified location and will display the forecast of that area. In case the forecast is not loaded, it will send the alert specifying the issue based on user's query. When searching for the information it might take some time (in some occassions) to fetch the details from the api. In any case, if the details are not available please wait and try to search the information again.
+When the user enters the specified location in the text box, the details get stored in the session storage which inturn will be displayed in the dropdown and so if the session gets refreshed or closed, the details will be erased.
+The location button which is right beside the search button uses geolocation api to get the information of the user's location (on click) and then when it recieves the location, it passes the latitude and the longitude coordinates in the weatherapi and fetches the forecast of the user.
+
+<!--  Steps to access the project -->
+unzip the folder as it is in your desktop.
+Open and run index.html.
+If you are unable to access the styles and want to see the applied styles then all the styles are stored in style.css as utility classes
+
+copy paste this in the terminal when you run the code in order to see the styles applied
+npx @tailwindcss/cli -i ./style.css -o ./output.css --watch
+
--- main.js
+++ main.js
@@ -1,28 +1,80 @@
 "use strict"; 
 // selecting all the dom elements to be manipulated
-let main = document.getElementById("main");
-let valueSearch = document.getElementById("valueSearch");
-let city = document.getElementById("city");
-let flag = document.getElementById("flag");
-let currentTemperature = document.getElementById("current-temperature");
-let temperaturelogo = document.getElementById("temperature-img");
-let currentDescription = document.getElementById("current-description");
-let currentClouds = document.getElementById("current-clouds");
-let currentHumidity = document.getElementById("current-humidity");
-let currentWindSpeed = document.getElementById("current-WindSpeed");
-let first_date = document.getElementById("first_date") 
-let first_image = document.getElementById("first_image") 
-let first_temp = document.getElementById("first_temp") 
-let first_cloud = document.getElementById("first_cloud") 
-let first_humidity = document.getElementById("first_humidity") 
-let first_wind = document.getElementById("first_wind") 
+const main = document.getElementById("main");
+const valueSearch = document.getElementById("valueSearch");
+
+// items stored in the main body (the main weather forecast)
+const city = document.getElementById("city");
+const currentTemperature = document.getElementById("current-temperature");
+const temperaturelogo = document.getElementById("temperature-img");
+const currentDescription = document.getElementById("current-description");
+const currentClouds = document.getElementById("current-clouds");
+const currentHumidity = document.getElementById("current-humidity");
+const currentWindSpeed = document.getElementById("current-WindSpeed");
+
+// items added in the first day's forecast
+const first_date = document.getElementById("first_date"); 
+const first_image = document.getElementById("first_image"); 
+const first_temp = document.getElementById("first_temp");
+const first_humidity = document.getElementById("first_humidity"); 
+const first_wind = document.getElementById("first_wind");
+
+// items added in the second day's forecast
+const second_date = document.getElementById("second_date");
+const second_image = document.getElementById("second_image");
+const second_temp = document.getElementById("second_temp");
+const second_humidity = document.getElementById("second_humidity");
+const second_wind = document.getElementById("second_wind");
+
+// items added in the third day's forecast
+const third_date = document.getElementById("third_date");
+const third_image = document.getElementById("third_image");
+const third_temp = document.getElementById("third_temp");
+const third_humidity = document.getElementById("third_humidity");
+const third_wind = document.getElementById("third_wind");
 // creating and submitting a form
-let form = document.getElementById("form");
+const form = document.getElementById("form");
+const dropdown = document.querySelector(".dropdown")
+const dropdown_list = document.querySelector(".dropdown-list")
+
+const cityNamesList = JSON.parse(sessionStorage.getItem("cityNamesList")) || [];
+
+// this eventlistener will add the dropdown on click event
+document.addEventListener("click",(event)=>{
+    if (event.target ==valueSearch){
+        dropdown.style.display = "block";
+        createDropdown();
+    }
+    else{
+        
+        dropdown.style.display = "none";
+    }
+    
+})
+
+//  this function is used for creating the dropdown and inserting the dropdown items in dom.
+const createDropdown = function(){
+dropdown_list.innerHTML = ""; // Clear previous items before appending new ones
+if (cityNamesList.length>0){
+    
+    cityNamesList.forEach((city) => {
+            let newCity = document.createElement("li"); // creating the list item for dropdown
+            newCity.classList.add("dropdown-item");
+            newCity.textContent = city;
+
+         newCity.addEventListener("click", function () {
+                valueSearch.value = city; 
+                dropdown.style.display = "none"; // Hide dropdown after selection
+            });
+
+        dropdown_list.append(newCity) // appending the list item to the dom
+    });
+}
+};
 
 /* this form takes the user input (on submit) and searches for the specified city 
-based on that input using the openweathermap api. 
+based on that input using the weather api. 
 If found then it will return the weather conditions of the location as a response*/
-
 form.addEventListener("submit", (event)=>
 {
 event.preventDefault() // prevents the reloading of the webpage on click
@@ -31,29 +83,56 @@ if (valueSearch.value!==" "){
     searchWeather();
 }
 else{
-    // addes the shake animation (error) if the input is not filled
+    // addes the shake animation on error if the input is not filled
     main.classList.add("error");
     alert("Please enter a city name!");
-    // removes the error class after 1 sec.
+    // removes the animation after 1 sec.
     setTimeout(()=> main.classList.remove("error"),1000); 
 }
 });
 
+// Api key
 let url = `http://api.weatherapi.com/v1/forecast.json?key=7df05d21f7544cdfaa991117252004&days=7&aqi=no&alerts=no`;
 
-/* fetches the data based on the url mentioned above
+
+/* The following function fetches the data based on the url mentioned above (api key)
 (fetches the weather information) and then converts it 
 into json object so that the retrieved string can be manipulated*/
+const searchWeather = async ()=>{
+    try{
+    const response = await fetch(url + "&q= " + valueSearch.value) // explicitly searches for the city provided by user
+    const data =  await response.json() // converting it into json
+       
+        if (data && data.location){ // if the city is found (since data is not empty(user has entered an input))
+            displayWeather(data) // sends data to displayWeather function
+            // storing the city names in dropdown
+           if (!cityNamesList.includes(valueSearch.value.trim().toLowerCase())){ // checks if the citynames exist in the cityNamesList array
+                cityNamesList.push(valueSearch.value.trim()); // If not then it pushes the value in the array
+                sessionStorage.setItem("cityNamesList", JSON.stringify(cityNamesList)); // stores the values in sessionStorage
+            }
 
-const searchWeather = ()=>{
-    fetch(url + "&q= " + valueSearch.value) // explicitly searches for the city provided by user
-    .then((Response)=> Response.json() // converting it into json
-    .then((data)=>{ 
-        console.log(data);
-        if (!data == " "){ // if the city is found (since data is not empty)
-            city.textContent = data.location.name;
-            // changing the flag image based on user input
-            // flag.setAttribute("src", `https://flagsapi.com/${data.sys.country}/shiny/64.png`)
+               // clearing out the input
+        valueSearch.value = " ";
+        }
+        // sends an alert if the user has entered invalid output
+        else{
+            main.classList.add("error");
+            alert("The city provided does not exist!")
+            setTimeout(()=> main.classList.remove("error"),1000);
+        }
+       
+    }
+      // throws an error if the data couldn't be fetched
+    catch(error){ 
+        console.error("Error fetching data:", error);
+        alert("Cannot get the details")}
+    };
+
+
+// function used for setting up the data and showing it in the dom
+function displayWeather(data){
+     city.textContent = data.location.name;
+
             
             // Changing the temperature logo based on the location provided
             temperaturelogo.setAttribute("src", data.current.condition.icon)
@@ -73,33 +152,49 @@ const searchWeather = ()=>{
 
             // setting upcoming forecast
             const forecastday =  data.forecast.forecastday;
-            // tomorrow's forecast
-            first_date.textContent = forecastday[0].date;
-            first_image.setAttribute("src" , forecastday[0].day.condition.icon);
-            first_temp.innerHTML = `<span>${forecastday[0].day.avgtemp_c}<sup>o</sup>C</span>`;
-            
-            // day after tomorrow's forecast
-            second_date.textContent = forecastday[1].date;
-            second_image.setAttribute("src" , forecastday[1].day.condition.icon);
-            second_temp.innerHTML = `<span>${forecastday[1].day.avgtemp_c}<sup>o</sup>C</span>`;
-            
-            // third day's forecast
-            third_date.textContent = forecastday[2].date;
-            third_image.setAttribute("src" , forecastday[2].day.condition.icon);
-            third_temp.innerHTML = `<span>${forecastday[2].day.avgtemp_c}<sup>o</sup>C</span>`;
-        }
-        else{
-            main.classList.add("error");
-            alert("The city provided does not exist!")
-            setTimeout(()=> main.classList.remove("error"),1000);
-        }
-        // clearing out the input
-        valueSearch.value = " ";
-    } )).catch((error)=>{ 
-        console.error("Error fetching data:", error);
-        alert("Cannot get the details")})
-};
-// default value
+            // comming weeks forecast
+            const elements = [
+    { date: first_date, image: first_image, temp: first_temp, wind: first_wind, humidity: first_humidity },
+    { date: second_date, image: second_image, temp: second_temp, wind: second_wind, humidity: second_humidity },
+    { date: third_date, image: third_image, temp: third_temp, wind: third_wind, humidity: third_humidity }
+];
+// looping through each object in elements to set it's data in dom
+for (let i = 0; i < elements.length; i++) {
+    elements[i].date.textContent = forecastday[i].date; // setting date on each element
+    elements[i].image.setAttribute("src", forecastday[i].day.condition.icon); // setting image on each element
+    elements[i].temp.innerHTML = `<span>${forecastday[i].day.avgtemp_c}<sup>o</sup>C</span>`; // setting temperature
+    elements[i].wind.textContent = forecastday[i].day.maxwind_kph; // setting wind speed
+    elements[i].humidity.textContent = forecastday[i].day.avghumidity; // setting humidity
+}
+
+}
+// getting user's weather forecast
+const userlocation = document.querySelector(".location-btn");
+userlocation.addEventListener("click", function(){
+    const success =(pos)=>{ // if the search is successful
+        const latitude = pos.coords.latitude;
+        const longitude = pos.coords.longitude;
+        getWeatherbyCurrentLocation(latitude,longitude)
+    }
+    const error = () =>{ // if it failed to get the user's position
+        console.error("Error fetching details")
+        alert("Couldn't get your location")
+    }
+    navigator.geolocation.getCurrentPosition(success,error)
+})
+// This function takes latitude and longitude from the geolocation and then uses weatherapi to fetch the details of the provided lat and lon.
+const getWeatherbyCurrentLocation = async(lat,lon) =>{
+    try{
+         const response = await fetch(`http://api.weatherapi.com/v1/forecast.json?key=7df05d21f7544cdfaa991117252004&q=${lat},${lon}&days=7&aqi=no&alerts=no`);
+        const data = await response.json();
+        
+        displayWeather(data); // sending the data to displayWeather
+    }
+    catch{
+    alert("Sorry couldn't fetch the details of your location")
+    }
+}
+// default value (default forecast when the page loads)
 const initApp = () =>{
     valueSearch.value = "Mumbai";
     searchWeather();
